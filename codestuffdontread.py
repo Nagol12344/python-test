4d505fce49592066c09e11c59db752d7e8a2dcf9
Logan Someone <63481547+Nagol12344@users.noreply.github.com>
2021-04-27 12:52:22 -0400

    cool

---
+++ codestuffdontread.py
@@ -0,0 +1,11 @@
+import time
+
+def end():
+    print('You have failed')
+    time.sleep(2)
+    print('Please wait...')
+    time.sleep(7)
+    print('Now ending all code, please wait...')
+    time.sleep(10)
+    print('Done!')
+    time.sleep(2)
