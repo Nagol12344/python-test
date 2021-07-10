6aaaa36d1c6fbf504ae02533948058107257c821
Logan Someone <loganqqq@outlook.com>
2021-07-10 17:44:02 -0400

    start part 2

--- notmain.py
+++ notmain.py
@@ -2,16 +2,10 @@ import time
 import random
 from codestuffdontread import end
 
-def spam(a,b):
-    for b in range(76978689):
-        print(a)
-
 time.sleep(2)
-print('This part coming soon...')
-time.sleep(3)
-print('What could it be')
-time.sleep(3)
-spam2 = input('Type whatever you want ')
+print('You have been runing for hours')
 time.sleep(4)
-spam3 = random.random()
-spam(spam2,spam3)
+print('do you want to stop?')
+time.sleep(2)
+print('type 1 for yes')
+print('type 2 for no')
