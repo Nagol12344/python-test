0e1909ffaa5abb1d9c1a30b93de3da2aaa9639d4
Logan Someone <loganqqq@outlook.com>
2021-07-10 18:15:12 -0400

    do some more work on part 2

--- notmain.py
+++ notmain.py
@@ -2,6 +2,8 @@ import time
 import random
 from codestuffdontread import end
 
+atu = 'What are you doing'
+
 time.sleep(2)
 print('You have been runing for hours')
 time.sleep(4)
@@ -9,3 +11,8 @@ print('do you want to stop?')
 time.sleep(2)
 print('type 1 for yes')
 print('type 2 for no')
+sn = input(atu)
+if sn == '1':
+    print('you stop running')
+elif sn == '2':
+    print('you keep running')
