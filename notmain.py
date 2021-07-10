41a164f57d05c8abbe5c2174aab8f581ead60e19
Logan Someone <loganqqq@outlook.com>
2021-07-10 17:06:23 -0400

    add something

--- notmain.py
+++ notmain.py
@@ -1,6 +1,17 @@
 import time
+import random
 from codestuffdontread import end
 
+def spam(a,b):
+    for b in range(76978689):
+        print(a)
 
 time.sleep(2)
-print('Hello')
+print('This part coming soon...')
+time.sleep(3)
+print('What could it be')
+time.sleep(3)
+spam2 = input('Type whatever you want ')
+time.sleep(4)
+spam3 = random.random()
+spam(spam2,spam3)
