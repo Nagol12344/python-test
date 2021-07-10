507294f9594f6a5cff68813fc9850889ed7cb67b
Logan Someone <loganqqq@outlook.com>
2021-07-10 18:45:03 -0400

    finish sn2 and start sn1

--- notmain.py
+++ notmain.py
@@ -14,5 +14,13 @@ print('type 2 for no')
 sn = input(atu)
 if sn == '1':
     print('you stop running')
+    time.sleep(3)
+    print('you sit down')
+    time.sleep(2)
+    print('You take a quick nap and wake up about 2 hours later')
+    time.sleep(3)
+    print('what do you want to do now?')
 elif sn == '2':
     print('you keep running')
+    print('after 2 more hours you run off a cliff')
+    end()
